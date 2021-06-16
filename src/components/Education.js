bbe00b60eb82a364455609137a36c4b4d980baaa
soni kumari <67049547+SoniKumari30@users.noreply.github.com>
2021-06-16 23:52:37 +0530

    portfolio

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,11 +6,11 @@ class Education extends Component {
         return (
             <div className="condiv">
             <h1 className="subtopic">My Education :-</h1>
-            <Widecard title="B-Tech (Computer Science Engineering)" where="Amritsar College Of Engineering And Technology" from="July 2016" to="july 2020" marks="7.3 CGPA"/>
+            <Widecard title="B-Tech(Computer Science Engineering)" where="Amritsar College Of Engineering And Technology" from="July 2016" to="july 2020" marks="7.3 CGPA"/>
             </div>
             )
         }
-    }
+    }//
     
 export default Education
     
--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,6 @@ class Home extends Component {
         }
         
     }
-    
+    //
     export default Home
     
